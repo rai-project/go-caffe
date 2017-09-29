cf1c8bd19f6d5e73834a709baf56a2d11601684d
Cheng Li <cli99@illinois.edu>
2017-09-29 14:10:30 -0500

    add openblas loc to flags

--- lib.go
+++ lib.go
@@ -1,11 +1,10 @@
 package caffe
 
-// #cgo LDFLAGS: -lcaffe -lstdc++ -lglog -lboost_system  
-// #cgo CXXFLAGS: -DBLAS=open -std=c++11 -I${SRCDIR}/cbits -O3 -Wall -DCPU_ONLY=1 
+// #cgo LDFLAGS: -lcaffe -lstdc++ -lglog -lboost_system
+// #cgo CXXFLAGS: -DBLAS=open -std=c++11 -I${SRCDIR}/cbits -O3 -Wall -DCPU_ONLY=1
 // #cgo CXXFLAGS: -Wno-sign-compare -Wno-unused-function
-// #cgo darwin CXXFLAGS: -I/opt/caffe/include
-// #cgo darwin LDFLAGS: -L/opt/caffe/lib
+// #cgo darwin CXXFLAGS: -I/opt/caffe/include -I/usr/local/opt/openblas/include
+// #cgo darwin LDFLAGS: -L/opt/caffe/lib  -L/usr/local/opt/openblas/lib
 // #cgo ppc64le CXXFLAGS: -I/home/carml/frameworks/caffe/include
 // #cgo ppc64le LDFLAGS: -L/home/carml/frameworks/caffe/lib
 import "C"
-
