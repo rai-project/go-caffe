1d3369c56ecb0e60a9f2edfc5febf69c57a94f31
Ayushi Patel <ayushi@wirelessprv-10-195-156-47.near.illinois.edu>
2018-12-08 14:16:21 -0600

    add example

---
+++ examples/batch/preprocess.py
@@ -0,0 +1,8 @@
+transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
+transformer.set_transpose('data', (2, 0, 1))
+transformer.set_mean('data', np.array([104,117,123])) # mean pixel
+transformer.set_raw_scale('data', 255)  # the reference model operates on images in [0,255] range instead of [0,1]
+transformer.set_channel_swap('data', (2,1,0))  # the reference model has channels in BGR order instead of RGB
+net.blobs['data'].reshape(1,3,300,300)
+transformed_image = transformer.preprocess('data', image)
+net.blobs['data'].data[...] = transformed_image
